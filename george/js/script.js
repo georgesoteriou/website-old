a6234dbc31e32da7b35a8580c3f0a0e5f3e451c4
gs2617 <gs2617@ic.ac.uk>
2018-08-01 15:06:01 +0300

    Updated gitgraph

--- george/js/script.js
+++ george/js/script.js
@@ -36,55 +36,53 @@ $(function(){
   gitgraph.template.commit.message.font = "normal 16pt Arial"
 
   var commits = [
-    " - Initial Commit (26/05/1997)",
-    " - Completed Primary School @ Junior School Nicosia",
-    " - Accepted @ English School Nicosia",
-    " - Started Highschool @ English School Nicosia",
-    " - Passed Ham Radio exam and got a Lisence (5B4AJZ)",
-    " - Elected for Ham Radio Club (5B4ES) President for 4 years",
-    " - Volunteered and Helped organise Hack{Cyprus} with CEL",
-    " - Created and Organized Hack{Cyprus}Code School with CEL",
-    " - Created and was President of Tecnology club for 2 years",
-    " - Started working as Web Developer @ Think Learning Centre",
-    " - Completed A-Level: Double Maths with A*, A",
-    " - Completed A-Level: Computing with A*",
-    " - Completed A-Level: Physics with A",
-    " - Joined Cyprus Army as Corporal",
-    " - Created Think Learning Centre Website",
-    " - Completed Army Service",
-    " - Accepted @ Imperial Collage London",
-    " - Started Computing @ Imperial Collage London",
-    " - Completed Year 1, Term 1",
-    " - Volunteered for ICHack18",
-    " - Took part in Google Hashcode 2018",
-    " - Run and Elected for DoCSoc WebMaster 2018-19"];
+  " - Initial Commit (26/05/1997)",
+  " - Completed Primary School @ Junior School Nicosia",
+  " - Accepted @ English School Nicosia",
+  " - Started Highschool @ English School Nicosia",
+  " - Passed Ham Radio exam and got a Lisense (5B4AJZ)",
+  " - Elected for Ham Radio Club (5B4ES) President for 4 years",
+  " - Volunteered and Helped organise Hack{Cyprus} with CEL",
+  " - Created and Organized Hack{Cyprus}Code School with CEL",
+  " - Created and was President of Tecnology club for 2 years",
+  " - Started working as Web Developer @ Think Learning Centre",
+  " - Completed A-Level: Double Maths with A*, A",
+  " - Completed A-Level: Computing with A*",
+  " - Completed A-Level: Physics with A",
+  " - Joined Cyprus Army as Corporal",
+  " - Created Think Learning Centre Website",
+  " - Completed Army Service",
+  " - Accepted @ Imperial Collage London",
+  " - Started Computing @ Imperial Collage London",
+  " - Volunteered for ICHack18",
+  " - Took part in Google Hashcode 2018",
+  " - Run and Elected for DoCSoc WebMaster 2018-19",
+  " - Completed Year 1"];
 
-    var master = gitgraph.branch("master")
-    .commit(commits[0])
-    .commit(commits[1])
-    .commit(commits[2]);
-    var English = master.branch("English School")
-    .commit(commits[3]);
-    master.commit(commits[4]);
-    English.commit(commits[5]);
-    master.commit(commits[6])
-    .commit(commits[7]);
-    English.commit(commits[8]);
-    master.commit(commits[9]);
-    var Think = master.branch("Think Learning Centre");
-    English.commit(commits[10])
-    .commit(commits[11])
-    .commit(commits[12])
-    .merge(master);
-    master.commit(commits[13]);
-    Think.commit(commits[14]);
-    master.commit(commits[15])
-    .commit(commits[16]);
-    var Imperial = master.branch("Imperial Collage");
-    Imperial.commit(commits[17])
-    .commit(commits[18])
-    .commit(commits[19]);
-    master.commit(commits[20]);
-    Imperial.commit(commits[21]);
-
-  });
+  var master = gitgraph.branch("master")
+  .commit(commits[0])
+  .commit(commits[1])
+  .commit(commits[2]);
+  var English = master.branch("English School")
+  .commit(commits[3]);
+  master.commit(commits[4]);
+  English.commit(commits[5]);
+  master.commit(commits[6])
+  .commit(commits[7]);
+  English.commit(commits[8]);
+  master.commit(commits[9]);
+  var Think = master.branch("Think Learning Centre");
+  English.commit(commits[10])
+  .commit(commits[11])
+  .commit(commits[12])
+  .merge(master);
+  master.commit(commits[13]);
+  Think.commit(commits[14]);
+  master.commit(commits[15])
+  .commit(commits[16]);
+  var Imperial = master.branch("Imperial Collage");
+  Imperial.commit(commits[17])
+  .commit(commits[18]);
+  master.commit(commits[19]);
+  Imperial.commit(commits[20]).commit(commits[21]);
+});
