cbe2e18bf99e206fc59ac5353f5a0cac514a7ae0
George Soteriou <george@pop-os.localdomain>
2019-09-19 23:15:18 +0100

    timeline update

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,63 @@
-$(function(){
+$(function() {
   //GitGraph
-  var gitgraph = new GitGraph({author: ""});
-  gitgraph.template.colors = ["white","#ff8000","#94C7B2", "#4FBCF3"];
-  gitgraph.template.commit.message.font = "normal 16pt Arial"
+  var gitgraph = new GitGraph({ author: "" });
+  gitgraph.template.colors = [
+    "white",
+    "#ff8000",
+    "#94C7B2",
+    "#4FBCF3",
+    "#225590"
+  ];
+  gitgraph.template.commit.message.font = "normal 16pt Arial";
 
-  var commits = [
-  " - Initial Commit (26/05/1997)",
-  " - Completed Primary School @ Junior School Nicosia",
-  " - Accepted @ English School Nicosia",
-  " - Started Highschool @ English School Nicosia",
-  " - Passed Ham Radio exam and got a License (5B4AJZ)",
-  " - Elected for Ham Radio Club (5B4ES) President for 4 years",
-  " - Volunteered and Helped organise Hack{Cyprus} with CEL",
-  " - Created and Organized Hack{Cyprus}Code School with CEL",
-  " - Created and was President of Tecnology club for 2 years",
-  " - Started working as Web Developer @ Think Learning Centre",
-  " - Completed A-Level: Double Maths with A*, A",
-  " - Completed A-Level: Computing with A*",
-  " - Completed A-Level: Physics with A",
-  " - Joined Cyprus Army as Corporal",
-  " - Created Think Learning Centre Website",
-  " - Completed Army Service",
-  " - Accepted @ Imperial College London",
-  " - Started Computing @ Imperial College London",
-  " - Volunteered for ICHack18",
-  " - Took part in Google Hashcode 2018",
-  " - Run and Elected for DoCSoc WebMaster 2018-19",
-  " - Completed Year 1"];
-
-  var master = gitgraph.branch("master")
-  .commit(commits[0])
-  .commit(commits[1])
-  .commit(commits[2]);
-  var English = master.branch("English School")
-  .commit(commits[3]);
-  master.commit(commits[4]);
-  English.commit(commits[5]);
-  master.commit(commits[6])
-  .commit(commits[7]);
-  English.commit(commits[8]);
-  master.commit(commits[9]);
+  var master = gitgraph
+    .branch("master")
+    .commit(" - Initial Commit (26/05/1997)")
+    .commit(" - Complete Primary School @ Junior School Nicosia")
+    .commit(" - Get Accepted @ English School Nicosia");
+  var English = master
+    .branch("English School")
+    .commit(" - Start Highschool @ English School Nicosia");
+  master.commit(" - Pass Ham Radio exam and get a License (5B4AJZ)");
+  English.commit(
+    " - Get Elected for Ham Radio Club (5B4ES) President for 4 years"
+  );
+  master
+    .commit(" - Volunteer and Help organise Hack{Cyprus} with CEL")
+    .commit(" - Create and Organize Hack{Cyprus}Code School with CEL");
+  English.commit(" - Create and was President of Tecnology club for 2 years");
+  master.commit(" - Start working as Web Developer @ Think Learning Centre");
   var Think = master.branch("Think Learning Centre");
-  English.commit(commits[10])
-  .commit(commits[11])
-  .commit(commits[12])
-  .merge(master);
-  master.commit(commits[13]);
-  Think.commit(commits[14]);
-  master.commit(commits[15])
-  .commit(commits[16]);
+  English.commit(" - Complete A-Level: Double Maths with A*, A")
+    .commit(" - Complete A-Level: Computing with A*")
+    .commit(" - Complete A-Level: Physics with A")
+    .merge(master);
+  Think.commit(" - Create Think Learning Centre Website");
+  Think.merge(master);
+  master
+    .commit(" - Join Cyprus Army as Corporal")
+    .commit(" - Complete Army Service")
+    .commit(" - Get Accepted @ Imperial College London");
   var Imperial = master.branch("Imperial College");
-  Imperial.commit(commits[17])
-  .commit(commits[18]);
-  master.commit(commits[19]);
-  Imperial.commit(commits[20]).commit(commits[21]);
+  Imperial.commit(" - Start Computing @ Imperial College London");
+  Imperial.commit(" - Volunteer for ICHack18");
+  master.commit(" - Take part in Google Hashcode 2018");
+  var DoCSoc = Imperial.branch("DoCSoc");
+  DoCSoc.commit(" - Run and get elected for DoCSoc WebMaster 2018-19");
+  Imperial.commit(" - Complete Year 1");
+  DoCSoc.commit(" - Create Sponsors Portal v1");
+  DoCSoc.commit(" - Organize ICHack 2019");
+  DoCSoc.commit(" - Organize Google Hashcode 2019 at Imperial");
+  DoCSoc.merge(Imperial);
+  DoCSoc = Imperial.branch("DoCSoc");
+  DoCSoc.commit(" - Run and get elected for DoCSoc WebMaster 2018-19");
+  master.commit(" - Work at Goldman Sachs as Spring Technology Intern");
+  Imperial.commit(" - Complete Year 2");
+  var Cisco = master.branch("Cisco");
+  Cisco.commit(" - Start work at Cisco as Platform Engineer Intern");
+  Cisco.commit(" - Create AWS Cisco Kubernetes Cluster");
+  Cisco.commit(" - Create GCP Cisco Kubernetes cluster");
+  Cisco.commit(" - Get MeetPaaS (Webex Teams) to work on Clusters");
+  Cisco.merge(master);
+  DoCSoc.commit(" - Create Sponsors Portal v2");
 });
