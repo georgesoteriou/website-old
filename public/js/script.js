4536bdaa5ce2a75717b230af3c00ce846800bf16
George Soteriou <george@pop-os.localdomain>
2019-09-19 23:47:48 +0100

    final bugfix

--- public/js/script.js
+++ public/js/script.js
@@ -9,36 +9,32 @@ $(function() {
     "#225590",
     "#009edc"
   ];
-  gitgraph.template.commit.message.font = "normal 16pt Arial";
+  gitgraph.template.commit.message.font = "normal 15pt Arial";
 
-  var master = gitgraph
-    .branch("master")
-    .commit(" - Initial Commit (26/05/1997)")
-    .commit(" - Complete Primary School @ Junior School Nicosia");
-  var English = master
-    .branch("English School")
-    .commit(" - Get Accepted @ English School Nicosia")
-    .commit(" - Start Highschool @ English School Nicosia");
+  var master = gitgraph;
+  master.branch("master");
+  master.commit(" - Initial Commit (26/05/1997)");
+  master.commit(" - Complete Primary School @ Junior School Nicosia");
+  var English = master;
+  English.branch("English School");
+  English.commit(" - Get Accepted @ English School Nicosia");
+  English.commit(" - Start Highschool @ English School Nicosia");
   master.commit(" - Pass Ham Radio exam and get a License (5B4AJZ)");
-  English.commit(
-    " - Get Elected for Ham Radio Club (5B4ES) President for 4 years"
-  );
-  master
-    .commit(" - Volunteer and Help organise Hack{Cyprus} with CEL")
-    .commit(" - Create and Organize Hack{Cyprus}Code School with CEL");
+  English.commit(" - Get Elected for Radio Club (5B4ES) President (4 years)");
+  master.commit(" - Volunteer and Help organise Hack{Cyprus} with CEL");
+  master.commit(" - Create and Organize Hack{Cyprus}Code School with CEL");
   English.commit(" - Create and was President of Tecnology club for 2 years");
   var Think = master.branch("Think Learning Centre");
-  Think.commit(" - Start working as Web Developer @ Think Learning Centre");
-  English.commit(" - Complete A-Level: Double Maths with A*, A")
-    .commit(" - Complete A-Level: Computing with A*")
-    .commit(" - Complete A-Level: Physics with A")
-    .merge(master);
+  Think.commit(" - Start working as Web Developer");
+  English.commit(" - Complete A-Level: Double Maths with A*, A");
+  English.commit(" - Complete A-Level: Computing with A*");
+  English.commit(" - Complete A-Level: Physics with A");
+  English.merge(master);
   Think.commit(" - Create Think Learning Centre Website");
   Think.merge(master);
-  master
-    .commit(" - Join Cyprus Army as Corporal")
-    .commit(" - Complete Army Service")
-    .commit(" - Get Accepted @ Imperial College London");
+  master.commit(" - Join Cyprus Army as Corporal");
+  master.commit(" - Complete Army Service");
+  master.commit(" - Get Accepted @ Imperial College London");
   var Imperial = master.branch("Imperial College");
   Imperial.commit(" - Start Computing @ Imperial College London");
   Imperial.commit(" - Volunteer for ICHack18");
